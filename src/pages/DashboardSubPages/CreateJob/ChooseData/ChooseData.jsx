fb7a702d81c02e65375bc27445820daff62846cb
snake-py <vabischannel@gmail.com>
2021-12-24 00:45:00 +0100

    plotly change color

--- src/pages/DashboardSubPages/CreateJob/ChooseData/ChooseData.jsx
+++ src/pages/DashboardSubPages/CreateJob/ChooseData/ChooseData.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect, useRef } from 'react';
+import { useSelector } from '@redux/hooks';
 import * as d3 from 'd3';
 import Plot from 'react-plotly.js';
+import { darkTheme, lightTheme } from '../../../../styler';
+
 // https://plotly.com/javascript/reference/#layout
 // https://github.com/plotly/react-plotly.js/blob/master/README.md
 export default function ChooseData() {
+  const app = useSelector(state => state.app);
   return (
     <div className='alignCenter w-100'>
-    
+
       <Plot
         data={[{ type: 'bar', x: [1, 2, 3, 4, 5, 6, 7, 8], y: [2, 5, 3, 1, 7, 6, 10, 4] }]}
 
@@ -14,10 +17,10 @@ export default function ChooseData() {
           width: 1100,
           height: 500,
           title: 'A Fancy Plot',
-          plot_bgcolor: '#222222',
-          paper_bgcolor: '#222222',
+          plot_bgcolor: app.darkMode ? darkTheme.palette.background.default : lightTheme.palette.background.default,
+          paper_bgcolor: app.darkMode ? darkTheme.palette.background.default : lightTheme.palette.background.default,
           font: {
-            color: '#ffffff'
+            color:  app.darkMode ? darkTheme.palette.text.primary : lightTheme.palette.background.secondary
           },
         }}
           showLegend={false}
--- src/styler.js
+++ src/styler.js
@@ -1,7 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-// import variables from './static/scss/base/_variables.module.scss';
-import { yellow, teal, blue, indigo, grey} from '@mui/material/colors';
-
+import { yellow, teal, blue, indigo, grey } from '@mui/material/colors';
 
 export const darkTheme = createTheme({
   palette: {
@@ -9,17 +7,16 @@ export const darkTheme = createTheme({
     primary: yellow,
     secondary: teal,
     background: {
-      default: "#222222"
+      default: '#222222',
     },
     text: {
-      primary: "#ffffff",
-      secondary: "#000000"
+      primary: '#ffffff',
+      secondary: '#000000',
     },
     divider: {
-      primary: "#ffffff",
-      secondary: "#000000"
+      primary: '#ffffff',
+      secondary: '#000000',
     },
-
   },
 });
 
@@ -29,16 +26,15 @@ export const lightTheme = createTheme({
     primary: blue,
     secondary: indigo,
     background: {
-      default: "#e4f0e2"
+      default: '#e4f0e2',
     },
     text: {
-      primary: "#000000",
-      secondary: "#ffffff"
+      primary: '#000000',
+      secondary: '#ffffff',
     },
     divider: {
-      primary: "#000000",
-      secondary: "#ffffff"
+      primary: '#000000',
+      secondary: '#ffffff',
     },
-
   },
 });
