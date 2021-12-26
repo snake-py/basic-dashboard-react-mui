61d517fc0b8b2e53ed3127b1f4d03b168a637eed
snake-py <vabischannel@gmail.com>
2021-12-26 14:21:31 +0100

    restyling

--- src/pages/DashboardSubPages/Job/JobDetails/JobDetails.tsx
+++ src/pages/DashboardSubPages/Job/JobDetails/JobDetails.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Wrapper from '@components/wrapper';
+
 export default function JobDetails() {
   const { id } = useParams();
+  
   return (
     <div className='main-container'>
       <h1>Job Details {id}</h1>
+        <Wrapper styles={{ width: '100%' }}>
+        
+        </Wrapper>
     </div>
   );
 }
