202bb6fea38bf1bc001499bffe16caeb3056ceda
snake-py <vabischannel@gmail.com>
2022-01-10 01:12:32 +0100

    clean up .env

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import { ViteAliases } from 'vite-aliases'
 import EnvironmentPlugin from 'vite-plugin-environment'
 
-import ENV from './ENV'
 
 export default defineConfig({
-  plugins: [react(), ViteAliases(), EnvironmentPlugin(ENV)],
+  plugins: [react(), ViteAliases(), EnvironmentPlugin('all', { loadEnvFiles: true })],
 })
